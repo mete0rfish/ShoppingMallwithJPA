48de8836b4b0fe7cb9bd435551ecd5803b05671f
mereorfish <sungwon326@naver.com>
2022-12-08 16:08:55 +0900

    2장 JPA

---
+++ src/main/java/com/example/demo/entity/Item.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity;
+
+import com.example.demo.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="item")
+@Getter
+@Setter
+@ToString
+public class Item {
+    @Id
+    @Column(name="item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;       // 상품 코드
+
+    @Column(nullable = false, length = 50)
+    private String itemNm; // 상품명
+
+    @Column(name="price", nullable = false)
+    private int price;     // 가격
+
+    @Column(nullable = false)
+    private int stockNumber; // 재고 수량
+
+    @Lob // BLOB, CLOB 타입 매핑
+    @Column(nullable = false)
+    private String itemDetail; // 상품 상세 설명
+
+    @Enumerated(EnumType.STRING) // enum 타입 매핑
+    private ItemSellStatus itemSellStatus; // 상품 판매 상태 (enum 타입 클래스)
+
+    private LocalDateTime regTime;   // 등록 시간
+
+    private LocalDateTime updateTime; // 수정 시간
+}
---
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -0,0 +1,29 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long>,
+        QuerydslPredicateExecutor<Item> {
+
+    List<Item> findByItemNm(String itemNm); // find + (엔티티 이름)생략가능 + By + 변수이름
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price); // 순서대로
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price); // 내림차순
+
+
+    @Query("select i from Item i where i.itemDetail like " + // like 다음에 띄어쓰기 해야됨
+            "%:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+
+
+
+}
